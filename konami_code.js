b9678a6538668dafc7cd9b226e12a188828d0518
Landen Danyluk <landendanyluk@gmail.com>
2018-06-10 00:39:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,25 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-  var index = 0;
 
-  document.addEventListener('keydown', function(e) {
-    keyName = e.which
 
-    if (keyName === codes[index]) {
-      index++;
-
-      if (index === codes.length) {
-        alert("Whoop whoop");
-      }
-
-    } else {
-      index = 0;
+  function onKonamiCode(cb) {
+  var input = '';
+  var key = '38384040373937396665';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    if (input === key) {
+      return cb();
     }
-  })
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
 
-
-
+onKonamiCode(function () {alert('\o/')})
 
 }
